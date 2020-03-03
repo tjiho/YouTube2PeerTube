86d9b3a804a1b5dc8a891b168a1873c8391774fa
Tjiho <tom.darboux@laposte.net>
2020-03-03 13:27:55 +0100

    implement logging

--- youtube2peertube.py
+++ youtube2peertube.py
@@ -12,7 +12,40 @@ import mimetypes
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 import utils
 
-def get_video_data(channel_id,peertube_channel):
+
+# Init du logger 
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+# création de l'objet logger qui va nous servir à écrire dans les logs
+logger = logging.getLogger()
+# on met le niveau du logger à DEBUG, comme ça il écrit tout
+logger.setLevel(logging.DEBUG)
+
+# création d'un formateur qui va ajouter le temps, le niveau
+# de chaque message quand on écrira un message dans le log
+formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+# création d'un handler qui va rediriger une écriture du log vers
+# un fichier en mode 'append', avec 1 backup et une taille max de 1Mo
+file_handler = RotatingFileHandler('activity.log', 'a', 1000000, 1)
+# on lui met le niveau sur DEBUG, on lui dit qu'il doit utiliser le formateur
+# créé précédement et on ajoute ce handler au logger
+
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# création d'un second handler qui va rediriger chaque écriture de log
+# sur la console
+stream_formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(stream_formatter)
+logger.addHandler(stream_handler)
+
+
+def get_video_data(channel_id,peertube_channel,peertube_instance_url):
     yt_rss_url = "https://www.youtube.com/feeds/videos.xml?user=" + channel_id
     feed = fp.parse(yt_rss_url)
     channel_lang = feed["feed"]["title_detail"]["language"]
@@ -20,35 +53,23 @@ def get_video_data(channel_id,peertube_channel):
     # clear any existing queue before start
     queue = []
     
-    peertube_videos_url = "https://tube.fede.re"+"/api/v1/video-channels/"+peertube_channel+"/videos"
-
+    peertube_videos_url = peertube_instance_url+"/api/v1/video-channels/"+peertube_channel+"/videos"
+    logger.info("Fetching peertube videos from channel %s ...",peertube_channel)
     peertube_videos = requests.get(peertube_videos_url)
-    if peertube_videos.status_code == 200:
+
+    if handle_peertube_result(peertube_videos):
         for pos, i in enumerate(reversed(entries)):
             same_videos = [True for peertube_video in peertube_videos.json()['data'] if i['title'] == peertube_video['name'] ]
             if not same_videos:
-                print(i['title'] + " add to queue")
+                logger.debug(" -" + i['title'] + " add to queue")
                 queue.append(i)
+    else:
+        logger.warning('Peertube api return %s code', peertube_videos.status_code)
+    
+    if not queue:
+        logger.info("all videos already exist in channel %s",peertube_channel)
     # read contents of channels_timestamps.csv, create list object of contents
     return queue, channel_lang
-
-def download_yt_video(queue_item, dl_dir, channel_conf):
-    url = queue_item["link"]
-    dl_dir = dl_dir + channel_conf["name"]
-    try:
-        video = pafy.new(url)
-        streams = video.streams
-        #for s in streams:
-            #print(s.resolution, s.extension, s.get_filesize, s.url)
-        best = video.getbest(preftype=channel_conf["preferred_extension"])
-        filepath = dl_dir + "/"+ queue_item["yt_videoid"] + "." + channel_conf["preferred_extension"]
-        #TODO: implement resolution logic from config, currently downloading best resolution
-        best.download(filepath=filepath, quiet=False)
-
-    except:
-        pass
-        # TODO: check YT alternate URL for video availability
-        # TODO: print and log exceptions
 
 def save_metadata(queue_item, dl_dir, channel_conf):
     dl_dir = dl_dir + channel_conf["name"]
@@ -65,15 +86,6 @@ def save_metadata(queue_item, dl_dir, channel_conf):
     # save raw metadata JSON string
     metadata.write(str(queue_item))
     metadata.close()
-
-def save_thumbnail(queue_item, dl_dir, channel_conf):
-    dl_dir = dl_dir + channel_conf["name"]
-    thumb = str(queue_item["media_thumbnail"][0]["url"])
-    extension = thumb.split(".")[-1]
-    thumb_file = dl_dir + "/" + queue_item["yt_videoid"] + "." + extension
-    # download the thumbnail
-    urlretrieve(thumb, thumb_file)
-    return extension
 
 def get_pt_auth(channel_conf):
     # get variables from channel_conf
@@ -115,11 +127,10 @@ def handle_peertube_result(request_result):
     if request_result.status_code < 300:
         return True
     else:
-        print(request_result)
+        logger.warning("request error: %s",request_result.status_code)
         return False
     
 def pt_http_import(channel_conf, queue_item, access_token,yt_lang):
-    # Adapted from Prismedia https://git.lecygnenoir.info/LecygneNoir/prismedia
     pt_api = channel_conf["peertube_instance"] + "/api/v1"
     yt_video_url = queue_item["link"]
     # TODO: use the alternate link if video not found error occurs
@@ -128,30 +139,26 @@ def pt_http_import(channel_conf, queue_item, access_token,yt_lang):
     channel_id = str(get_pt_channel_id(channel_conf))
     language = utils.set_pt_lang(yt_lang, channel_conf["default_lang"])
     category = utils.set_pt_category(channel_conf["pt_channel_category"])
-    # We need to transform fields into tuple to deal with tags as
-    # MultipartEncoder does not support list refer
-    # https://github.com/requests/toolbelt/issues/190 and
-    # https://github.com/requests/toolbelt/issues/205
+    
     fields = {
-            "name":queue_item["title"],
-            "licence":"1",
+        "name":queue_item["title"],
+        "licence":"1",
         "description":description,
         "nsfw":channel_conf["nsfw"],
         "channelId":channel_id,
         "originallyPublishedAt":queue_item["published"],
         "category":category,
-        "privacy":"1",
+        "privacy":str(channel_conf["pt_privacy"]),
         "language":language,
         "commentsEnabled":channel_conf["comments_enabled"],
         "targetUrl":yt_video_url,
-        #"thumbnailfile":get_file(thumb_file),
-        #"previewfile":get_file(thumb_file),
         "waitTranscoding":'false'
     }
-    if channel_conf["pt_tags"] != "":
+    
+    if channel_conf["pt_tags"]:
         fields["tags"] = channel_conf["pt_tags"]
     else:
-        print("you have no tags in your configuration file for this channel")
+        logger.warning("you have no tags in your configuration file for this channel")
 
     headers = {
         'Content-Type': 'application/json',
@@ -161,40 +168,18 @@ def pt_http_import(channel_conf, queue_item, access_token,yt_lang):
     return handle_peertube_result(requests.post(pt_api + "/videos/imports", data=json.dumps(fields), headers=headers))
         
 
-def log_upload_error(yt_url,channel_conf):
-    error_file = open("video_errors.csv", "a")
-    error_file.write(channel_conf['name']+","+yt_url+"\n")
-    error_file.close()
-    print("error !")
-
 def run_steps(conf):
-    # TODO: logging
     channel = conf["channel"]
-    # run loop for every channel in the configuration file
     global_conf = conf["global"]
-    channel_counter = 0
+    # run loop for every channel in the configuration file
     for c in channel:
-        print("\n")
-        channel_id = channel[c]["channel_id"]
-        channel_conf = channel[str(channel_counter)]
-        video_data = get_video_data(channel_id,channel[c]["peertube_channel"])
-        queue = video_data[0]
-        yt_lang = video_data[1]
+        channel_conf = channel[c]
+        queue,yt_lang = get_video_data(channel_conf["channel_id"],channel_conf["peertube_channel"],channel_conf["peertube_instance"])
         if len(queue) > 0:
-            # download videos, metadata and thumbnails from youtube
             access_token = get_pt_auth(channel_conf)
-            # upload videos, metadata and thumbnails to peertube
             for queue_item in queue:
-                print("mirroring " + queue_item["link"] + " to Peertube using HTTP import...")
+                logger.info("mirroring " + queue_item["link"] + " to Peertube using HTTP import...")
                 pt_result = pt_http_import(channel_conf, queue_item, access_token, yt_lang)
-                print("waiting...")
-                sleep(3)
-                if pt_result:
-                    print("done !")
-                else:
-                    log_upload_error(queue_item["link"],channel_conf)
-
-        channel_counter += 1
 
 def run(run_once=True):
     #TODO: turn this into a daemon
@@ -205,6 +190,7 @@ def run(run_once=True):
         while True:
             poll_frequency = int(conf["global"]["poll_frequency"]) * 60
             run_steps(conf)
+            print("all jobs done... sleeping")
             sleep(poll_frequency)
 
 if __name__ == "__main__":
